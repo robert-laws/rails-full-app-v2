b1ac412e30a6d6d80ab1e3f6a0fa07e6da9393ab
Robert Laws <roblaws@me.com>
2018-03-01 10:26:20 +0300

    add secure password, user index, show routes, seed data

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
-  
-
-  get 'users/index'
-
-  get 'users/show'
-
+  # Root
   root 'static#home'
+
+  # Users
+  get '/users', to: 'users#index'
+  get '/user/:id', to: 'users#show', as: 'user'
 end
