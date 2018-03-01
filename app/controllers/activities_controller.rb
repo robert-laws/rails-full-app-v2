bf7e481fede84b2d23675976eeedab7b293528bf
Robert Laws <roblaws@me.com>
2018-03-01 14:01:24 +0300

    implement nested controller and view logic for locations -> activities

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,9 +1,29 @@
 class ActivitiesController < ApplicationController
   def index
-    @activities = Activity.all
+    if params[:location_id] 
+      # case of nested url path
+      @location = Location.find_by(id: params[:location_id])
+      if @location.nil?
+        redirect_to activities_path, alert: "Location not found"
+      else
+        @activities = @location.activities
+      end
+    else 
+      # case of no nested url path
+      @activities = Activity.all
+    end
   end
 
   def show
-    @activity = Activity.find(params[:id])
+    if params[:location_id]
+      # case of nested url path
+      @location = Location.find_by(id: params[:location_id])
+      @activity = @location.activities.find_by(id: params[:id])
+      if @activity.nil?
+        redirect_to location_activities_path(@location), alert: "Activity not found"
+      end
+    else
+      @activity = Activity.find(params[:id])
+    end
   end
 end
