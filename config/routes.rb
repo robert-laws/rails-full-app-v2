5c9d25792bac5d352e3b390f4f2bc8c9ee0d2d6a
Robert Laws <roblaws@me.com>
2018-03-01 10:44:09 +0300

    add users new/form, users#new action, /signup route

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   # Root
   root 'static#home'
 
+  # Sessions / New User
+  get '/signup', to: 'users#new', as: 'new_user'
+
   # Users
   get '/users', to: 'users#index'
   get '/user/:id', to: 'users#show', as: 'user'
