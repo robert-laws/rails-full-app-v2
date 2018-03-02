f5fe8c35701c26c5768dc8d435b96d89f2fe3f14
Robert Laws <roblaws@me.com>
2018-03-02 20:05:05 +0300

    refactor to new class method find_or_create_by_omniauth

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,22 +4,11 @@ class SessionsController < ApplicationController
   end
 
   def create
-    if request.env["omniauth.auth"]
+    if auth_hash = request.env["omniauth.auth"]
       # login with OAuth
-      oauth_email = request.env["omniauth.auth"]["info"]["email"]
-      oauth_username = request.env["omniauth.auth"]["info"]["email"].split("@")[0]
-      if @user = User.find_by(email: oauth_email)
-        session[:user_id] = @user.id
-        redirect_to user_path(@user), notice: "You have successfully logged in"
-      else
-        @user = User.new(username: oauth_username, email: oauth_email, password: SecureRandom.hex)
-        if @user.save
-          session[:user_id] = @user.id
-          redirect_to user_path(@user), notice: "You have successfully logged in"
-        else
-          raise user.errors.full_messages.inspect
-        end
-      end
+      @user = User.find_or_create_by_omniauth(auth_hash)
+      session[:user_id] = @user.id
+      redirect_to user_path(@user)
     else
       # login with username and password
       @user = User.find_by(username: params[:user][:username])
