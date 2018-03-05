a461a3d51b2b694bb0f12884bc667d7f137889c9
Robert Laws <roblaws@me.com>
2018-03-05 11:02:34 +0300

    Revert "Merge branch 'user-accounts'"
    This reverts commit 229afe2b108495aae40cbc9a0f7811ea26b98fb2, reversing
    changes made to e7e727f6ffcad37c0b32b2fd0819574293a48627.

--- app/models/user.rb
+++
@@ -1,8 +0,0 @@
-class User < ActiveRecord::Base
-  has_secure_password
-
-  validates :username, presence: true
-  # validates :username, uniqueness: true
-
-  validates :email, presence: true
-end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,7 @@
-User.destroy_all
-
-@user_1 = User.create(first_name: "Bob", last_name: "Cobb", email: "bobcobb@net.com", username: "bobcobb", password: "hello1")
-@user_2 = User.create(first_name: "Hal", last_name: "Hope", email: "halhope@net.com", username: "halhope", password: "hello1")
-@user_3 = User.create(first_name: "Kal", last_name: "Kool", email: "kalkool@net.com", username: "kalkool", password: "hello1")
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
